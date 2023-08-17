ed04ff84679382a5c59faa1d82e43abb5bc7a32c
daniel Kabagambe <danielkabagambe@gmail.com>
2023-08-17 11:16:39 +0300

    Labour Supply

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Service from "../components/Service";
-import videoFile1 from "../img/VID-20230815-WA0025.mp4";
-import videoFile2 from "../img/VID-20230815-WA0023.mp4";
+// import videoFile1 from "../img/VID-20230815-WA0025.mp4";
+// import videoFile2 from "../img/VID-20230815-WA0023.mp4";
 import videoFile3 from "../img/VID-20230815-WA0024.mp4";
 
 const servicesData = [
@@ -15,7 +15,7 @@ const servicesData = [
     serviceName: "Helpers at Sites",
     description:
       "Make your construction sites more efficient with our experienced helpers. Our team of site assistants is well-versed in providing support to construction projects. Whether it's lifting, carrying, or organizing materials, our helpers ensure a smooth workflow at your sites.",
-    videoUrl: videoFile2,
+    videoUrl: "https://www.youtube.com/embed/M32JRrVoVhQ?controls=0&showinfo=0",
   },
   {
     serviceName: "Sales Personnels",
@@ -27,25 +27,25 @@ const servicesData = [
     serviceName: "Technicians",
     description:
       "When it comes to technical expertise, our technicians have you covered. From repairing equipment to handling complex installations, our skilled technicians bring a wealth of experience and knowledge to tackle a variety of technical challenges.",
-    videoUrl: videoFile2,
+    videoUrl: "https://www.youtube.com/embed/7Gazj3_hDEU?controls=0&showinfo=0",
   },
   {
     serviceName: "Cleaners",
     description:
       "Maintain a clean and organized environment with our professional cleaning services. Our team of cleaners ensures that your spaces are spotless and hygienic, providing a comfortable and welcoming atmosphere for employees and visitors alike.",
-    videoUrl: videoFile1,
+    videoUrl: "https://www.youtube.com/embed/48vawUlO7ag?controls=0&showinfo=0",
   },
   {
     serviceName: "Hotel Booking",
     description:
       "Planning a getaway? Let us take care of your hotel booking needs. With our extensive network of accommodations, we can help you find the perfect hotel for your travel preferences. Whether it's a luxurious resort or a cozy inn, we make sure your stay is comfortable and enjoyable.",
-    videoUrl: videoFile2,
+    videoUrl: "https://www.youtube.com/embed/FyPGUImfkOQ?controls=0&showinfo=0",
   },
   {
     serviceName: "Driving",
     description:
       "Need a reliable and experienced driver? Our driving services offer safe and efficient transportation solutions. Whether you require a chauffeur for special occasions or a skilled driver for your daily commute, our professionals ensure a smooth and secure journey.",
-    videoUrl: videoFile3,
+    videoUrl: "https://www.youtube.com/embed/DvF_AoGm-wE?controls=0&showinfo=0",
   },
 ];
 
