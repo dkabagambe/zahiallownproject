36b2c0f2822ade0beb139892b63da01ec71b9ad1
daniel Kabagambe <danielkabagambe@gmail.com>
2023-08-18 14:27:05 +0300

    Zahi Allown Project Management Services

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import videoFile from "../img/home.mp4"; // Import the video file
 const Home = () => {
   return (
     <div className="video-container">
+      <h1>Zahi Allown Project Management Services</h1>
       <video autoPlay loop muted>
         <source src={videoFile} type="video/mp4" />
         Your browser does not support the video tag.
